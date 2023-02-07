ef9511bfa3ff1dc3b43567dbc91d76aecd768c83
Celio Santos <celio.diasjunior@gmail.com>
2023-02-07 15:06:57 -0300

    FIX testing sets

--- src/simgenes/__init__.py
+++
@@ -1,7 +0,0 @@
-from simgenes import random_gene
-from simgenes import batch_random
-from simgenes import entropy
-from simgenes import get_prot
-from simgenes import get_len
-from simgenes import det_prop
-from simgenes import check_internal_stops
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 def test_entropy():
-    from simgenes import entropy
+    from simgenes.simgenes import entropy
     res = {'ATGCTGAGATGAGAGATTT': (1.7990156706551217, 2.0, 0.8995078353275608),
            'AAAAAAAAAAAAAAAAAAA': (-0.0, -0.0, 0),
            'MAKLKHIPQTTTTAHH': (2.952819531114783, 3.169925001442312, 0.9315108495536184),
@@ -10,7 +10,7 @@ def test_entropy():
 
 
 def test_format_orf():
-    from simgenes import format_orf
+    from simgenes.simgenes import format_orf
     seq = 'XXXXXX'
     seq = format_orf(seq)
     a = seq[0:3]
@@ -23,29 +23,29 @@ def test_format_orf():
 
 
 def test_internal_stops():
-    from simgenes import check_internal_stops
+    from simgenes.simgenes import check_internal_stops
     seq = 'ATGAAAAAATAAAAATGATGTTTTCCCCGCTAG'
     stops = check_internal_stops(seq)
     assert [3, 5, 10] == stops
 
 
-def test_prot():    
-    from simgenes import get_prot
+def test_prot():
+    from simgenes.simgenes import get_prot
     seq = ['', 'ATGTTAA', 'ATGAAATTTAAATAG', 'KKK']
     res = [get_prot(x) for x in seq]
     expect = ['', 'MLX', 'MKFK*', 'X']
     assert res == expect
-    
-    
+
+
 def test_len():
-    from simgenes import get_len
+    from simgenes.simgenes import get_len
     x = get_len(10,1000)
     assert isinstance(x, int)
     assert 10 <= x <= 1000
- 
+
 
 def checkseq(L, gc, trans):
-    from simgenes import random_gene
+    from simgenes.simgenes import random_gene
     s, p = random_gene(L=L, gc=gc, trans=trans)
     if trans:
         assert p != None, 'Did not translate sequence'
@@ -74,13 +74,13 @@ def test_rg():
         for g in gc:
             for t in trans:
                 checkseq(L=m, gc=g, trans=t)
-                    
-    
+
+
 def test_batch():
     import os
     import lzma
     from Bio import SeqIO
-    from simgenes import batch_random
+    from simgenes.simgenes import batch_random
     batch_random(ofile='output_testseq',
                  L=(30, 300),
                  GC=50.8,
@@ -102,3 +102,5 @@ def test_batch():
     l = sum(l)
     assert len(hs) == 100, 'Produced less aa sequences than expected'
     assert l == 0, 'Produced empty aa sequences'
+    os.remove('output_testseq.faa.xz')
+    os.remove('output_testseq.fna.xz')
